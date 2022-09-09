1900f508e8451a5dc29f6cb092550607e30efeb2
Martin Aguayo <martineduardoaguay@gmail.com>
2022-09-09 15:17:57 -0300

    xd

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos create')
+
+@section('content')
+  <h1>En esta pagina vamos a crear un curso</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantilla')
+
+@section('tittle', 'Home')
+
+@section('content')
+  <h1>Bienvenido a la pagina principal</h1>
+@endsection
+
